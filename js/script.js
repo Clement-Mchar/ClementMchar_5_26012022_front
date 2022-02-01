5b662e864ec3a92be75c852d56e09ee6e0449af8
Clement M'char <clement.mchar@gmail.com>
2022-02-01 18:44:45 +0100

    création de script.js, affichage des produits sur l'index

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const item = document.getElementById("items");
+
+fetch("http://localhost:3000/api/products")
+	.then((products) => {
+		if (products.ok) {
+			return products.json();
+		}
+	})
+	.then(function (products) {
+		let card = "";
+		for (let product of products) {
+			card += `<a href="./html/product.html?id=${product._id}">
+                    <article>
+                        <img src="${product.imageUrl}"alt="${product.altTxt}">
+                        <h3 class="productName">${product.name}</h3>
+                        <p class="productDescription">${product.description}</p>
+                    </article>
+                </a>`;
+		}
+		item.insertAdjacentHTML("afterbegin", card);
+	})
+
+	.catch(function (err) {
+		// Une erreur est survenue
+	});
